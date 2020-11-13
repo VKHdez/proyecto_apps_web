04239c5a3b1092bc8712a58898189cc4d761da2b
Víctor Hdez <atlante_infinito@outlook.com>
2020-11-13 14:14:42 -0600

    Creación de Proyecto | Aplicaciones Web Añadido de archivos de estructura principales siguiente el patrón MVC

---
+++ view/general_view/v_menu.php
@@ -0,0 +1,8 @@
+    <header>
+        <nav>
+            <ul class="menu">
+                <li class="menu-item">Home</li>
+                <li class="menu-item">Aout</li>
+            </ul>
+        </nav>
+    </header>
