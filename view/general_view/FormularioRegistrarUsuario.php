0b65a8e3e83d170588f67a24a20c065306a4c23c
BernardoPHP <berna_perez7@hotmail.com>
2020-11-17 22:38:08 -0600

    commit rapido

---
+++ view/general_view/FormularioRegistrarUsuario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- META -->
+    <meta charset="UTF-8">
+    <!-- LINK -->
+    <link rel="stylesheet" href="view/general_view/css/normalize.css">
+    <link rel="stylesheet" href="view/general_view/css/main_styles.css">
+    <link rel="stylesheet" href="view/general_view/css/menu_style.css">
+    <title>APUNT.ES</title>
+</head>
+
+<body>
+	<?php //require('controller/general_controller/c_menu.php'); ?>
+	<td style="padding-left:180px;">
+	<table width="370" border="0" cellpadding="0" cellspacing="0" class="bg5"><tr><td>
+	<form id="form2" name="form2" method="post" action="">
+	
+		<table width="85%" border="0" align="center" cellpadding="0" cellspacing="0">
+   			<tr><td class="style1"><h5 class="text3">Nombre  </h5></td>
+			<td align="center"><input name="nombretxt" type="text" class="input1"></td> </tr>
+   			<tr><td class="style1"><h5 class="text3">Apellido  </h5></td>
+			<td align="center"><input name="apellidotxt" type="text" class="input1"></td> 
+			<tr><td class="style1"><h5 class="text3">Password  </h5></td>
+			<td align="center"><input name="passwordtxt" type="password" class="input1"></td> 
+			<td><input name="Search2" type="submit" id="Search2" value="    Aceptar   "></td>
+			</tr></table>
+	</form></td>
+	</tr></table></td>
+   		
+</body>
+
+</html>
---
+++ view/general_view/formularioIniciarSesion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- META -->
+    <meta charset="UTF-8">
+    <!-- LINK -->
+    <link rel="stylesheet" href="view/general_view/css/normalize.css">
+    <link rel="stylesheet" href="view/general_view/css/main_styles.css">
+    <link rel="stylesheet" href="view/general_view/css/menu_style.css">
+    <title>APUNT.ES</title>
+</head>
+
+<body>
+	<td style="padding-left:180px;">
+	<table width="370" border="0" cellpadding="0" cellspacing="0" class="bg5"><tr><td>
+	<form id="form2" name="form2" method="post" action="">
+		<table width="85%" border="0" align="center" cellpadding="0" cellspacing="0">
+   		<tr><td class="style1"><h5 class="text3">Login  </h5></td>
+		<td align="center"><input name="login" type="text" class="input1"></td> </tr>
+   		<tr><td class="style1"><h5 class="text3">Password  </h5></td>
+		<td align="center"><input name="password" type="password" class="input1"></td> 
+		<td>�</td>
+		<td><input name="Search2" type="submit" id="Search2" value="    Aceptar   "></td>
+		</tr></table>
+	</form></td>
+	</tr></table></td>
+   
+</body>
+</html>
